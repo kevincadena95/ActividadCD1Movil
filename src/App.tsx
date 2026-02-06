484d4c4972e95d7cc0d7f8f358eb5263b60f06ff
kevincadena <kevgeek5@gmail.com>
2026-02-05 20:43:09 -0500

    ¨Ejercicio 1 realizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Ejercicio1 } from "./typescript/ejercicio1";
 
   const App = () => {
   return (
     <div className="mt-2">
       <h2>Ejercicios de la Actividad Contacto Docente</h2>
-      
+      <hr />
+    <Ejercicio1/>
     </div>
   )
 }
---
+++ src/typescript/ejercicio1.tsx
@@ -0,0 +1,17 @@
+
+export const Ejercicio1 = () => {
+
+    const lado: number = 5;
+    const areaCuadrado = (lado: number): number => {
+    return lado * lado;
+}
+
+    return (
+    <div> 
+        <h3 className="font-bold">Ejercicio 1:</h3> 
+        <span>El área de un cuadrado se calcula multiplicando la medida de uno de sus lados por sí misma.</span> <br /><br />
+        <span>Por lo tanto el area de un cuadrado que mide {lado} es: {areaCuadrado(lado)} </span>
+
+    </div>
+    )
+}
